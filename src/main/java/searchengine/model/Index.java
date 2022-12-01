dd30554b64c9515551fb5c905896821a7a7daf2e
StepanShushakov <step.sv@mail.ru>
2022-12-01 22:05:38 +0700

    liquibase-attached

--- src/main/java/searchengine/model/Index.java
+++ src/main/java/searchengine/model/Index.java
@@ -3,6 +3,7 @@ package searchengine.model;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "indexT")
 public class Index {
 
     @Id
@@ -10,12 +11,12 @@ public class Index {
     @Column(nullable = false)
     private int id;
 
-//    @OneToOne(fetch = FetchType.LAZY)
-//    private Page page;
-//
-//    @OneToOne(fetch = FetchType.LAZY)
-//    private Lemma lemma;
+    @OneToOne(fetch = FetchType.LAZY)
+    private Page page;
 
-    @Column(name = "lemma_rank", nullable = false)
+    @OneToOne(fetch = FetchType.LAZY)
+    private Lemma lemma;
+
+    @Column(name = "rankT", nullable = false)
     private float rank;
 }
