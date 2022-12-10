b2a434c5b46a6026fa88b918525144f18e4d033a
Степан Шушаков <step.sv@mail.ru>
2022-12-10 14:47:20 +0700

    deleted-injection-released-fjp-to-submit-redone

--- src/main/java/searchengine/model/PageRepository.java
+++ src/main/java/searchengine/model/PageRepository.java
@@ -1,6 +1,7 @@
 package searchengine.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 import searchengine.model.Page;
 import searchengine.model.Portal;
@@ -9,5 +10,5 @@ import java.util.List;
 
 public interface PageRepository extends JpaRepository<Page, Integer> {
     List<Page> findByPortalAndPath(@NonNull Portal portal, @NonNull String path);
-
+    void deleteByPortal(Portal portal);
 }
--- src/main/java/searchengine/services/IndexingServiceImpl.java
+++ src/main/java/searchengine/services/IndexingServiceImpl.java
@@ -10,14 +10,17 @@ import searchengine.RepositoriesFactory;
 import searchengine.SiteLinker;
 import searchengine.config.SitesList;
 import searchengine.dto.indexing.IndexingResponse;
-import searchengine.model.IndexStatus;
-import searchengine.model.PageRepository;
-import searchengine.model.Portal;
-import searchengine.model.PortalRepository;
+import searchengine.model.*;
 
+import javax.sound.sampled.Port;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 
 @Service
@@ -36,12 +39,30 @@ public class IndexingServiceImpl implements IndexingService{
     @Value("${jsoupFakePerformance.referrer}")
     private String referrer;
 
+    @Value("${spring.datasource.username}")
+    private String DBUserName;
+
+    @Value("${spring.datasource.password}")
+    private String DBPassword;
+
+    @Value("${spring.datasource.url}")
+    private String DBUrl;
+
+    private Connection connection;
+    private Statement statement;
+
     @Override
     public IndexingResponse startIndexing() {
         ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
         sites.getSites().forEach(site -> {
             Portal portal = portalRepository.findByNameAndUrl(site.getName(), site.getUrl());
             if (portal != null){
+                //pageRepository.deleteByPortal(portal);
+                try {
+                    deletePagesByPortal(portal);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);  //: будем возвращать ошибку
+                }
                 portalRepository.delete(portal);
             }
             Portal newPortal = new Portal();
@@ -52,7 +73,7 @@ public class IndexingServiceImpl implements IndexingService{
             newPortal.setStatusTime(new Date());
             portalRepository.save(newPortal);
             try {
-                pool.invoke(new SiteLinker(newPortal.getUrl()
+                pool.submit(new SiteLinker(newPortal.getUrl()
                         ,new URL(portalLink).getHost().replaceAll("^www.", "")
                         ,newPortal
                         ,new RepositoriesFactory(portalRepository, pageRepository)
@@ -64,10 +85,27 @@ public class IndexingServiceImpl implements IndexingService{
                 portalRepository.save(newPortal);
             }
         });
-
+        closeStatementConnection();
 
         IndexingResponse response = new IndexingResponse();
         response.setSites(sites);
         return response;
+    }
+
+    private void closeStatementConnection() {
+        try {
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+        }
+    }
+
+    private void deletePagesByPortal(Portal portal) throws SQLException {
+        if (this.connection == null) {
+            this.connection = (Connection) DriverManager.getConnection(DBUrl, DBUserName, DBPassword);
+            this.statement = connection.createStatement();
+        }
+        this.statement.execute("delete from page where site_id = " + portal.getId());
     }
 }
